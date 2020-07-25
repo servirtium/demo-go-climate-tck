27fc89704393b751d385659c1d33755fad422ac8
Duong Pham <tanphamhaiduong@gmail.com>
2020-07-25 22:51:51 +0700

    chore: add playback test back again

--- climate_test.go
+++ climate_test.go
@@ -19,20 +19,20 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-func serverMock() *httptest.Server {
+func serverPlaybackMock() *httptest.Server {
 	r := mux.NewRouter()
-	r.HandleFunc("/climateweb/rest/v1/country/annualavg/pr/{fromCCYY}/{toCCYY}/{countryISO}.xml", anualAvgHandlerRemote)
+	r.HandleFunc("/climateweb/rest/v1/country/annualavg/pr/{fromCCYY}/{toCCYY}/{countryISO}.xml", anualAvgHandlerPlayback)
 	srv := httptest.NewServer(r)
 	return srv
 }
 
-func anualAvgHandlerLocal(w http.ResponseWriter, r *http.Request) {
+func anualAvgHandlerPlayback(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	fromCCYY := vars["fromCCYY"]
 	toCCYY := vars["toCCYY"]
 	countryISO := vars["countryISO"]
 
-	data, err := ioutil.ReadFile(fmt.Sprintf("./mock/%s_%s_%s.xml", fromCCYY, toCCYY, countryISO))
+	data, err := ioutil.ReadFile(fmt.Sprintf("./mock/average_Rainfall_For_%s_From_%s_to_%s.md", strings.ToLower(countryISO), fromCCYY, toCCYY))
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write([]byte("Internal Server Error"))
@@ -42,6 +42,13 @@ func anualAvgHandlerLocal(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/xml")
 	_, _ = w.Write(data)
 	return
+}
+
+func serverRecordMock() *httptest.Server {
+	r := mux.NewRouter()
+	r.HandleFunc("/climateweb/rest/v1/country/annualavg/pr/{fromCCYY}/{toCCYY}/{countryISO}.xml", anualAvgHandlerRecord)
+	srv := httptest.NewServer(r)
+	return srv
 }
 
 func generateFileName(requestURLPath string) string {
@@ -91,7 +98,7 @@ func record(params recordData) {
 	}
 }
 
-func anualAvgHandlerRemote(w http.ResponseWriter, r *http.Request) {
+func anualAvgHandlerRecord(w http.ResponseWriter, r *http.Request) {
 	// Clone Request Body
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -140,10 +147,12 @@ func anualAvgHandlerRemote(w http.ResponseWriter, r *http.Request) {
 }
 
 type ClimateTestSuite struct {
-	client ClientImpl
+	recordClient   ClientImpl
+	directClient   ClientImpl
+	playbackClient ClientImpl
 	suite.Suite
-	remoteClient ClientImpl
-	serverMock   *httptest.Server
+	serverRecordMock   *httptest.Server
+	serverPlaybackMock *httptest.Server
 }
 
 func TestClimateTestSuite(t *testing.T) {
@@ -152,46 +161,65 @@ func TestClimateTestSuite(t *testing.T) {
 
 func (s *ClimateTestSuite) SetupTest() {
 	validate := validator.New()
-	s.serverMock = serverMock()
-	client := NewClient(http.DefaultClient, validate, s.serverMock.URL)
-	s.client = *client
-	remoteClient := NewClient(http.DefaultClient, validate, "http://climatedataapi.worldbank.org")
-	s.remoteClient = *remoteClient
+	directClient := NewClient(http.DefaultClient, validate, "http://climatedataapi.worldbank.org")
+	s.directClient = *directClient
+
+	s.serverRecordMock = serverRecordMock()
+	recordClient := NewClient(http.DefaultClient, validate, s.serverRecordMock.URL)
+	s.recordClient = *recordClient
+
+	s.serverPlaybackMock = serverPlaybackMock()
+	playbackClient := NewClient(http.DefaultClient, validate, s.serverPlaybackMock.URL)
+	s.playbackClient = *playbackClient
 }
 
 func (s *ClimateTestSuite) TestNewClient_Success() {
-	s.NotNil(s.client)
+	s.NotNil(s.directClient)
+	s.NotNil(s.recordClient)
+	s.NotNil(s.playbackClient)
+
 }
 
 func (s *ClimateTestSuite) TestNewGetRequestWithRelativeURL_Success() {
 	var (
-		input          = "/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml"
-		expected       = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", s.serverMock.URL)
-		expectedRemote = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", "http://climatedataapi.worldbank.org")
+		playbackInput  = "/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml"
+		playbackExpect = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", s.serverPlaybackMock.URL)
+		recordInput    = "/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml"
+		recordExpect   = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", s.serverRecordMock.URL)
+		directInput    = "/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml"
+		directExpected = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", "http://climatedataapi.worldbank.org")
 		ctx            = context.Background()
 	)
-	r, err := s.client.NewGetRequest(ctx, input)
-	s.Equal(expected, r.URL.String())
-	s.Nil(err)
-	r, err = s.remoteClient.NewGetRequest(ctx, input)
-	s.Equal(expectedRemote, r.URL.String())
-	s.Nil(err)
+	playbackRequest, playbackErr := s.playbackClient.NewGetRequest(ctx, playbackInput)
+	s.Equal(playbackExpect, playbackRequest.URL.String())
+	s.Nil(playbackErr)
+	recordRequest, recordErr := s.recordClient.NewGetRequest(ctx, recordInput)
+	s.Equal(recordExpect, recordRequest.URL.String())
+	s.Nil(recordErr)
+	directRequest, directErr := s.directClient.NewGetRequest(ctx, directInput)
+	s.Equal(directExpected, directRequest.URL.String())
+	s.Nil(directErr)
 }
 
 func (s *ClimateTestSuite) TestNewGetRequestWithAbsoluteURL_Success() {
 	var (
-		input          = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", s.serverMock.URL)
-		expected       = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", s.serverMock.URL)
-		remoteInput    = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", "http://climatedataapi.worldbank.org")
-		expectedRemote = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", "http://climatedataapi.worldbank.org")
-		ctx            = context.Background()
+		playbackInput    = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", s.serverRecordMock.URL)
+		playbackExpected = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", s.serverRecordMock.URL)
+		recordInput      = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", s.serverRecordMock.URL)
+		recordExpected   = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", s.serverRecordMock.URL)
+		directInput      = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", "http://climatedataapi.worldbank.org")
+		directExpected   = fmt.Sprintf("%s/climateweb/rest/v1/country/annualavg/pr/1980/1999/GBR.xml", "http://climatedataapi.worldbank.org")
+		ctx              = context.Background()
 	)
-	r, err := s.client.NewGetRequest(ctx, input)
-	s.Equal(expected, r.URL.String())
-	s.Nil(err)
-	remoteRq, err := s.remoteClient.NewGetRequest(ctx, remoteInput)
-	s.Equal(expectedRemote, remoteRq.URL.String())
-	s.Nil(err)
+	playbackRequest, playbackErr := s.playbackClient.NewGetRequest(ctx, playbackInput)
+	s.Equal(playbackExpected, playbackRequest.URL.String())
+	s.Nil(playbackErr)
+	recordRequest, recordErr := s.recordClient.NewGetRequest(ctx, recordInput)
+	s.Equal(recordExpected, recordRequest.URL.String())
+	s.Nil(recordErr)
+	directRequest, directErr := s.directClient.NewGetRequest(ctx, directInput)
+	s.Equal(directExpected, directRequest.URL.String())
+	s.Nil(directErr)
 }
 
 func (s *ClimateTestSuite) TestGetAnnualRainfall_Success() {
@@ -203,12 +231,15 @@ func (s *ClimateTestSuite) TestGetAnnualRainfall_Success() {
 		}
 		ctx = context.Background()
 	)
-	result, err := s.client.GetAnnualRainfall(ctx, input)
-	s.NotNil(result)
-	s.Nil(err)
-	result, err = s.remoteClient.GetAnnualRainfall(ctx, input)
-	s.NotNil(result)
-	s.Nil(err)
+	playbackResult, playbackErr := s.playbackClient.GetAnnualRainfall(ctx, input)
+	s.NotNil(playbackResult)
+	s.Nil(playbackErr)
+	recordResult, recordErr := s.recordClient.GetAnnualRainfall(ctx, input)
+	s.NotNil(recordResult)
+	s.Nil(recordErr)
+	directResult, directErr := s.directClient.GetAnnualRainfall(ctx, input)
+	s.NotNil(directResult)
+	s.Nil(directErr)
 }
 
 func (s *ClimateTestSuite) TestGetAnnualRainfall_Failed() {
@@ -221,12 +252,15 @@ func (s *ClimateTestSuite) TestGetAnnualRainfall_Failed() {
 		ctx      = context.Background()
 		expected = List{}
 	)
-	result, err := s.client.GetAnnualRainfall(ctx, input)
-	s.Equal(expected, result)
-	s.NotNil(err)
-	result, err = s.remoteClient.GetAnnualRainfall(ctx, input)
-	s.Equal(expected, result)
-	s.NotNil(err)
+	playbackResult, playbackErr := s.playbackClient.GetAnnualRainfall(ctx, input)
+	s.Equal(expected, playbackResult)
+	s.NotNil(playbackErr)
+	recordResult, recordErr := s.recordClient.GetAnnualRainfall(ctx, input)
+	s.Equal(expected, recordResult)
+	s.NotNil(recordErr)
+	directResult, directErr := s.directClient.GetAnnualRainfall(ctx, input)
+	s.Equal(expected, directResult)
+	s.NotNil(directErr)
 }
 
 func (s *ClimateTestSuite) TestCalculateAveAnual_Success() {
@@ -249,12 +283,15 @@ func (s *ClimateTestSuite) TestCalculateAveAnual_Success() {
 		toCCYY   = int64(1990)
 		expected = decimal.NewFromFloat32(10.5)
 	)
-	result, err := s.client.calculateAveAnual(list, fromCCYY, toCCYY)
-	s.Equal(expected.String(), result.String())
-	s.Nil(err)
-	result, err = s.remoteClient.calculateAveAnual(list, fromCCYY, toCCYY)
-	s.Equal(expected.String(), result.String())
-	s.Nil(err)
+	playbackResult, playbackErr := s.playbackClient.calculateAveAnual(list, fromCCYY, toCCYY)
+	s.Equal(expected.String(), playbackResult.String())
+	s.Nil(playbackErr)
+	recordResult, recordErr := s.recordClient.calculateAveAnual(list, fromCCYY, toCCYY)
+	s.Equal(expected.String(), recordResult.String())
+	s.Nil(recordErr)
+	directResult, directErr := s.directClient.calculateAveAnual(list, fromCCYY, toCCYY)
+	s.Equal(expected.String(), directResult.String())
+	s.Nil(directErr)
 }
 
 func (s *ClimateTestSuite) TestCalculateAveAnual_Failed() {
@@ -266,12 +303,15 @@ func (s *ClimateTestSuite) TestCalculateAveAnual_Failed() {
 		toCCYY   = int64(1990)
 		expected = decimal.NewFromInt(0)
 	)
-	result, err := s.client.calculateAveAnual(list, fromCCYY, toCCYY)
-	s.Equal(result, expected)
-	s.NotNil(err)
-	result, err = s.remoteClient.calculateAveAnual(list, fromCCYY, toCCYY)
-	s.Equal(result, expected)
-	s.NotNil(err)
+	playbackResult, playbackErr := s.playbackClient.calculateAveAnual(list, fromCCYY, toCCYY)
+	s.Equal(playbackResult, expected)
+	s.NotNil(playbackErr)
+	recordResult, recordErr := s.recordClient.calculateAveAnual(list, fromCCYY, toCCYY)
+	s.Equal(expected, recordResult)
+	s.NotNil(recordErr)
+	directResult, directErr := s.directClient.calculateAveAnual(list, fromCCYY, toCCYY)
+	s.Equal(expected, directResult)
+	s.NotNil(directErr)
 }
 
 func (s *ClimateTestSuite) TestAverageRainfallForGreatBritainFrom1980to1999Exists() {
@@ -279,12 +319,15 @@ func (s *ClimateTestSuite) TestAverageRainfallForGreatBritainFrom1980to1999Exist
 		ctx      = context.Background()
 		expected = float64(988.8454972331014)
 	)
-	result, err := s.client.GetAveAnnualRainfall(ctx, 1980, 1999, "gbr")
-	s.Equal(expected, result)
-	s.Nil(err)
-	result, err = s.remoteClient.GetAveAnnualRainfall(ctx, 1980, 1999, "gbr")
-	s.Equal(expected, result)
-	s.Nil(err)
+	playbackResult, playbackErr := s.playbackClient.GetAveAnnualRainfall(ctx, 1980, 1999, "gbr")
+	s.Equal(expected, playbackResult)
+	s.Nil(playbackErr)
+	recordResult, recordErr := s.recordClient.GetAveAnnualRainfall(ctx, 1980, 1999, "gbr")
+	s.Equal(expected, recordResult)
+	s.Nil(recordErr)
+	directResult, directErr := s.directClient.GetAveAnnualRainfall(ctx, 1980, 1999, "gbr")
+	s.Equal(expected, directResult)
+	s.Nil(directErr)
 }
 
 func (s *ClimateTestSuite) TestAverageRainfallForFranceFrom1980to1999Exists() {
@@ -292,12 +335,15 @@ func (s *ClimateTestSuite) TestAverageRainfallForFranceFrom1980to1999Exists() {
 		ctx      = context.Background()
 		expected = 913.7986955122727
 	)
-	result, err := s.client.GetAveAnnualRainfall(ctx, 1980, 1999, "fra")
-	s.Equal(expected, result)
-	s.Nil(err)
-	result, err = s.remoteClient.GetAveAnnualRainfall(ctx, 1980, 1999, "fra")
-	s.Equal(expected, result)
-	s.Nil(err)
+	playbackResult, playbackErr := s.playbackClient.GetAveAnnualRainfall(ctx, 1980, 1999, "fra")
+	s.Equal(expected, playbackResult)
+	s.Nil(playbackErr)
+	recordResult, recordErr := s.recordClient.GetAveAnnualRainfall(ctx, 1980, 1999, "fra")
+	s.Equal(expected, recordResult)
+	s.Nil(recordErr)
+	directResult, directErr := s.directClient.GetAveAnnualRainfall(ctx, 1980, 1999, "fra")
+	s.Equal(expected, directResult)
+	s.Nil(directErr)
 }
 
 func (s *ClimateTestSuite) TestAverageRainfallForEgyptFrom1980to1999Exists() {
@@ -305,12 +351,15 @@ func (s *ClimateTestSuite) TestAverageRainfallForEgyptFrom1980to1999Exists() {
 		ctx      = context.Background()
 		expected = float64(54.58587712129825)
 	)
-	result, err := s.client.GetAveAnnualRainfall(ctx, 1980, 1999, "egy")
-	s.Equal(expected, result)
-	s.Nil(err)
-	result, err = s.remoteClient.GetAveAnnualRainfall(ctx, 1980, 1999, "egy")
-	s.Equal(expected, result)
-	s.Nil(err)
+	playbackResult, playbackErr := s.playbackClient.GetAveAnnualRainfall(ctx, 1980, 1999, "egy")
+	s.Equal(expected, playbackResult)
+	s.Nil(playbackErr)
+	recordResult, recordErr := s.recordClient.GetAveAnnualRainfall(ctx, 1980, 1999, "egy")
+	s.Equal(expected, recordResult)
+	s.Nil(recordErr)
+	directResult, directErr := s.directClient.GetAveAnnualRainfall(ctx, 1980, 1999, "egy")
+	s.Equal(expected, directResult)
+	s.Nil(directErr)
 }
 
 func (s *ClimateTestSuite) TestAverageRainfallForGreatBritainFrom1985to1995DoesNotExist() {
@@ -318,12 +367,15 @@ func (s *ClimateTestSuite) TestAverageRainfallForGreatBritainFrom1985to1995DoesN
 		ctx      = context.Background()
 		expected = float64(0)
 	)
-	result, err := s.client.GetAveAnnualRainfall(ctx, 1985, 1995, "gbr")
-	s.Equal(expected, result)
-	s.Error(err)
-	result, err = s.remoteClient.GetAveAnnualRainfall(ctx, 1985, 1995, "gbr")
-	s.Equal(expected, result)
-	s.Error(err)
+	playbackResult, playbackErr := s.playbackClient.GetAveAnnualRainfall(ctx, 1985, 1995, "gbr")
+	s.Equal(expected, playbackResult)
+	s.Error(playbackErr)
+	recordResult, recordErr := s.recordClient.GetAveAnnualRainfall(ctx, 1985, 1995, "gbr")
+	s.Equal(expected, recordResult)
+	s.Error(recordErr)
+	directResult, directErr := s.directClient.GetAveAnnualRainfall(ctx, 1985, 1995, "gbr")
+	s.Equal(expected, directResult)
+	s.Error(directErr)
 }
 
 func (s *ClimateTestSuite) TestAverageRainfallForMiddleEarthFrom1980to1999DoesNotExist() {
@@ -331,10 +383,13 @@ func (s *ClimateTestSuite) TestAverageRainfallForMiddleEarthFrom1980to1999DoesNo
 		ctx      = context.Background()
 		expected = float64(0)
 	)
-	result, err := s.client.GetAveAnnualRainfall(ctx, 1980, 1999, "mde")
-	s.Equal(expected, result)
-	s.Error(err)
-	result, err = s.remoteClient.GetAveAnnualRainfall(ctx, 1980, 1999, "mde")
-	s.Equal(expected, result)
-	s.Error(err)
+	playbackResult, playbackErr := s.playbackClient.GetAveAnnualRainfall(ctx, 1980, 1999, "mde")
+	s.Equal(expected, playbackResult)
+	s.Error(playbackErr)
+	recordResult, recordErr := s.recordClient.GetAveAnnualRainfall(ctx, 1980, 1999, "mde")
+	s.Equal(expected, recordResult)
+	s.Error(recordErr)
+	directResult, directErr := s.directClient.GetAveAnnualRainfall(ctx, 1980, 1999, "mde")
+	s.Equal(expected, directResult)
+	s.Error(directErr)
 }
